06ecef1839b31985c4d793547d89e84140b099c0
yeray142 <yeraycorderocarrasco@gmail.com>
2023-09-30 12:28:25 +0200

    Updating Dijkstra's algorithm
    NOTE: There's a bug with Graf1000, where it returns 4 incorrect vertex distances. Check correction report.
    
    *Working on fixing that*

--- GraphApplication/GraphApplication/Dijkstra.cpp
+++ GraphApplication/GraphApplication/Dijkstra.cpp
@@ -16,11 +16,11 @@ void Dijkstra(CGraph& graph, CVertex *pStart)
 
 void DijkstraQueue(CGraph& graph, CVertex *pStart)
 {
-    for (auto it = begin(graph.m_Vertices); it != end(graph.m_Vertices); ++it) {
+    for (auto& vertex : graph.m_Vertices) {
         // Inicialitzar les distàncies dels vèrtexs a infinit excepte la del vèrtex start que serà 0
-        it->m_DijkstraDistance = numeric_limits<double>::max();
+        vertex.m_DijkstraDistance = numeric_limits<double>::max();
         // Marcar tots el vèrtex com no visitats (bool m_DijkstraVisit).
-        it->m_DijkstraVisit = false;
+        vertex.m_DijkstraVisit = false;
     }
     pStart->m_DijkstraDistance = 0;
 
@@ -42,11 +42,18 @@ void DijkstraQueue(CGraph& graph, CVertex *pStart)
         // Si no està marcat com visitat
         if (!va->m_DijkstraVisit) {
             // Recorre tots els veïns v de va i fer el següent
-                /* 
+            for (CEdge* e : va->m_Edges) {
+                /*
                 Si la distància de v és més grossa que la distància del vèrtex actual més la longitud de l’aresta que
                 els uneix, actualitzar la distància de v amb la distancia de va més la longitud de l’aresta que els
                 uneix i posar - lo a la cua.
                 */
+                CVertex* v = e->m_pDestination;
+                if (v->m_DijkstraDistance > (va->m_DijkstraDistance + e->m_Length)) {
+                    v->m_DijkstraDistance = va->m_DijkstraDistance + e->m_Length;
+                    queue.push(v);
+                }
+            }
 
             //Marcar va com visitat
             va->m_DijkstraVisit = true;
