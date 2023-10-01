cf5cc34d25f78950102c93199ee36fc98f842c10
yeray142 <yeraycorderocarrasco@gmail.com>
2023-10-01 18:13:25 +0200

    Fixing bugs
    Got 11 out of 10 on Dijkstra testing

--- GraphApplication/GraphApplication/Dijkstra.cpp
+++ GraphApplication/GraphApplication/Dijkstra.cpp
@@ -14,6 +14,20 @@ void Dijkstra(CGraph& graph, CVertex *pStart)
 // DijkstraQueue ===============================================================
 // =============================================================================
 
+struct Vertex
+{
+    CVertex* vertex;
+    double distance;
+
+    Vertex(CVertex* v, double d) : vertex(v), distance(d) {}
+};
+
+struct comparator {
+    bool operator()(Vertex V1, Vertex V2) {
+        return V1.distance > V2.distance;
+    }
+};
+
 void DijkstraQueue(CGraph& graph, CVertex *pStart)
 {
     for (auto& vertex : graph.m_Vertices) {
@@ -25,38 +39,35 @@ void DijkstraQueue(CGraph& graph, CVertex *pStart)
     pStart->m_DijkstraDistance = 0;
 
     // Posar a la cua el “vèrtex” start
-    struct comparator {
-        bool operator()(CVertex* pV1, CVertex* pV2) {
-            return pV1->m_DijkstraDistance > pV2->m_DijkstraDistance;
-        }
-    };
-    priority_queue<CVertex*, vector<CVertex*>, comparator> queue;
-    queue.push(pStart);
+    priority_queue<Vertex, vector<Vertex>, comparator> queue;
+    queue.push(Vertex(pStart, 0));
 
     // Mentre cua no buida
     while (!queue.empty()) {
         // va=primer element de la cua i treure'l de la cua.
-        CVertex* va = queue.top();
+        Vertex va = queue.top();
+        CVertex* pva = va.vertex;
         queue.pop();
 
         // Si no està marcat com visitat
-        if (!va->m_DijkstraVisit) {
+        if (!pva->m_DijkstraVisit) {
             // Recorre tots els veïns v de va i fer el següent
-            for (CEdge* e : va->m_Edges) {
+            for (CEdge* e : pva->m_Edges) {
                 /*
                 Si la distància de v és més grossa que la distància del vèrtex actual més la longitud de l’aresta que
                 els uneix, actualitzar la distància de v amb la distancia de va més la longitud de l’aresta que els
                 uneix i posar - lo a la cua.
                 */
                 CVertex* v = e->m_pDestination;
-                if (v->m_DijkstraDistance > (va->m_DijkstraDistance + e->m_Length)) {
-                    v->m_DijkstraDistance = va->m_DijkstraDistance + e->m_Length;
-                    queue.push(v);
+                double new_distance = pva->m_DijkstraDistance + e->m_Length;
+                if (v->m_DijkstraDistance > new_distance) {
+                    v->m_DijkstraDistance = new_distance;
+                    queue.push(Vertex(v, new_distance));
                 }
             }
 
             //Marcar va com visitat
-            va->m_DijkstraVisit = true;
+            pva->m_DijkstraVisit = true;
         }
     }
 }
