a866964c25f909adbf9374f3077ff6c244d606a6
yeray142 <yeraycorderocarrasco@gmail.com>
2023-10-20 09:09:41 +0200

    Finishing quickhull algorithm

--- GraphApplication/GraphApplication/ConvexHull.cpp
+++ GraphApplication/GraphApplication/ConvexHull.cpp
@@ -21,7 +21,7 @@ double AreaTriangle(CGPoint& a, CGPoint& b, CGPoint c) {
 
 CConvexHull ConvexHull(CVertex* a, CVertex* b, CConvexHull& punts)
 {
-	// Si Punts és Buit retornar la llista [A,B]
+	// Si Punts ï¿½s Buit retornar la llista [A,B]
 	if (punts.GetNVertices() == 0)
 	{
 		CConvexHull buit(punts.m_pGraph);
@@ -36,8 +36,8 @@ CConvexHull ConvexHull(CVertex* a, CVertex* b, CConvexHull& punts)
 	}
 
 	// Trobar el punt C de Punts que maximitzi AreaTriangle(A,C,B)
-	CVertex* c = punts.m_Vertices.front();
-	double max = AreaTriangle(a->m_Point, c->m_Point, b->m_Point);
+	CVertex* c = nullptr;
+	double max = 0.0;
 
 	for (CVertex* v : punts.m_Vertices)
 	{
@@ -50,7 +50,7 @@ CConvexHull ConvexHull(CVertex* a, CVertex* b, CConvexHull& punts)
 	}
 
 	// Separar punts en:
-	// Left: Punts que estan a l’esquerra de la recta AC
+	// Left: Punts que estan a lï¿½esquerra de la recta AC
 	// Right: Punts que estan a la dreta de CB
 	CConvexHull Left(punts.m_pGraph), Right(punts.m_pGraph);
 	for (CVertex* v : punts.m_Vertices)
@@ -62,16 +62,19 @@ CConvexHull ConvexHull(CVertex* a, CVertex* b, CConvexHull& punts)
 			Right.m_Vertices.push_back(v);
 	}
 
-	// Calcular les parts esquerra i dreta de l’envoltant convexa
+	// Calcular les parts esquerra i dreta de lï¿½envoltant convexa
 	CConvexHull CHLeft = ConvexHull(a, c, Left);
 	CConvexHull CHRight = ConvexHull(c, b, Right);
 
 	// Ajuntar les dos CHLeft i CHRight concatenant les llistes de punts. Compta en no repetir el punt C que pertanyen a les dues parts.
-	list<CVertex*>::iterator it = CHRight.m_Vertices.begin();
+	/*list<CVertex*>::iterator it = CHRight.m_Vertices.begin();
 	it++;
 	for (; it != CHRight.m_Vertices.end(); it++)
 		CHLeft.m_Vertices.push_back((*it));
-	// CHLeft.m_Vertices.pop_back();
+	*/
+	if (CHRight.m_Vertices.size() > 1) {
+		CHLeft.m_Vertices.insert(CHLeft.m_Vertices.end(), std::next(CHRight.m_Vertices.begin()), CHRight.m_Vertices.end());
+	}
 
 	return CHLeft;
 }
@@ -90,7 +93,7 @@ CConvexHull QuickHull(CGraph& graph)
 		return ch;
 	}
 
-	// Buscar dos punts extrems A i B que poden ser el que està més a l’esquerra i el que està més a la dreta.
+	// Buscar dos punts extrems A i B que poden ser el que estï¿½ mï¿½s a lï¿½esquerra i el que estï¿½ mï¿½s a la dreta.
 	CVertex* A = &graph.m_Vertices.front();
 	CVertex* B = &graph.m_Vertices.front();
 	for (CVertex& v : graph.m_Vertices)
@@ -102,6 +105,27 @@ CConvexHull QuickHull(CGraph& graph)
 			B = &v;
 	}
 
+	// En cas de que siguin vertexs verticals:
+	if (A->m_Name == B->m_Name)
+	{
+		for (CVertex& v : graph.m_Vertices)
+		{
+			if (v.m_Point.m_Y < A->m_Point.m_Y)
+				A = &v;
+
+			if (v.m_Point.m_Y > B->m_Point.m_Y)
+				B = &v;
+		}
+	}
+
+	// En cas de que esiguin superposats:
+	if (A->m_Name == B->m_Name && graph.GetNVertices() >= 2) 
+	{
+		CConvexHull ch(&graph);
+		ch.m_Vertices.push_back(A);
+		return ch;
+	}
+
 	// Separar el conjunt punts en els quals estan a sobre i els que estan a sota de la recta AB
 	CConvexHull Up(&graph), Down(&graph);
 	for (CVertex& v : graph.m_Vertices) {
@@ -111,16 +135,20 @@ CConvexHull QuickHull(CGraph& graph)
 			Down.m_Vertices.push_back(&v);
 	}
 
-	// Calcular el tros superior e inferior de l’envoltant convexa
+	// Calcular el tros superior e inferior de lï¿½envoltant convexa
 	CConvexHull CHUp = ConvexHull(A, B, Up);
 	CConvexHull CHDown = ConvexHull(B, A, Down);
 
-	// TODO: Ajuntar les dues CHUp i CHDown concatenant les llistes de punts. Compta en no repetir els punts A i B que pertanyen a les dues parts.
-	list<CVertex*>::iterator it = CHDown.m_Vertices.begin();
+	// Ajuntar les dues CHUp i CHDown concatenant les llistes de punts. Compta en no repetir els punts A i B que pertanyen a les dues parts.
+	/*list<CVertex*>::iterator it = CHDown.m_Vertices.begin();
 	it++;
 	for (; it != CHDown.m_Vertices.end(); it++)
 		CHUp.m_Vertices.push_back((*it));
 	CHUp.m_Vertices.pop_back();
-
+	*/
+	if (CHDown.m_Vertices.size() > 1) {
+		CHUp.m_Vertices.insert(CHUp.m_Vertices.end(), std::next(CHDown.m_Vertices.begin()), std::prev(CHDown.m_Vertices.end()));
+	}
+	
 	return CHUp;
 }
